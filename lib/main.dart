19ba1da2ef46cff96d233e85421f9085e5b92cf8
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-03-21 02:48:36 +0800

    UIs impelemented

--- lib/main.dart
+++ lib/main.dart
@@ -4,63 +4,129 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _selected = false;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Remote',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.grey,
         visualDensity: VisualDensity.adaptivePlatformDensity,
+        scaffoldBackgroundColor: Colors.grey[900],
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            Image.asset('assets/linux_PNG29.png', fit: BoxFit.fill, height: 50,),
+            Text('Controller'),
+          ],),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.airline_seat_individual_suite_rounded), label: Text('Sleep')),
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.power_off ), label: Text('Power Off'), color: _selected ? Colors.red : Colors.white,),
+                    ],
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.fromLTRB(15,0,15,0),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('VOLUMN', style: TextStyle(color: Colors.white),),
+                      Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.remove_circle), label: Text('Down')),
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.add_circle), label: Text('Up')),]),
+                    ],
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.fromLTRB(15,0,15,0),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text('BRIGHTNESS', style: TextStyle(color: Colors.white),),
+                      Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.remove_circle), label: Text('Down')),
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.add_circle), label: Text('Up')),]),
+                    ],
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.fromLTRB(15,0,15,0),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_next), label: Text('')),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(45.0,5,45.0,0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.fast_rewind ), label: Text('')),
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.pause ), label: Text('')),
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.fast_forward ), label: Text('')),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.skip_previous_sharp), label: Text('')),
+                    ],
+                  ),
+                ),
+                Padding(padding: const EdgeInsets.fromLTRB(15,0,15,0),
+                  child: Divider(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), 
     );
   }
 }
