5d55bf6e4ba697ac45dfc4e71495af68000beb05
Shihara-Dilshan <shiharadotadilshanid@gmail.com>
2021-03-30 03:08:49 -0700

    removed some deprecated elements

--- App/lib/main.dart
+++ App/lib/main.dart
@@ -6,10 +6,10 @@ void main() {
   runApp(Main());
 }
 
-class Main extends StatelessWidget{
+class Main extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-     return MaterialApp(
+  Widget build(BuildContext context) {
+    return MaterialApp(
       title: 'Flutter Remote',
       theme: ThemeData(
         primarySwatch: Colors.grey,
@@ -18,10 +18,9 @@ class Main extends StatelessWidget{
       ),
       debugShowCheckedModeBanner: false,
       home: MyApp(),
-      );
-  } 
+    );
+  }
 }
-
 
 class MyApp extends StatefulWidget {
   @override
@@ -37,19 +36,18 @@ class _MyAppState extends State<MyApp> {
 
   _handlePlay() async {
     String url = "";
-    if(!_isPlaying){
+    if (!_isPlaying) {
       url = "http://${currentIP}:${currentPORT}/play";
-    }else{
+    } else {
       url = "http://${currentIP}:${currentPORT}/pause";
     }
 
     final response = await http.get(url);
-    setState((){
+    setState(() {
       _isPlaying = !_isPlaying;
     });
 
     //var responseData = json.decode(response.body);
-
   }
 
   _volumnIncrease() async {
@@ -70,7 +68,7 @@ class _MyAppState extends State<MyApp> {
     final response = await http.get(url);
   }
 
-   _shutdown() async {
+  _shutdown() async {
     String url = "http://${currentIP}:${currentPORT}/pweroff";
 
     final response = await http.get(url);
@@ -88,7 +86,7 @@ class _MyAppState extends State<MyApp> {
     final response = await http.get(url);
   }
 
-   _fastfoward() async {
+  _fastfoward() async {
     String url = "http://${currentIP}:${currentPORT}/fastfaward";
 
     final response = await http.get(url);
@@ -102,140 +100,268 @@ class _MyAppState extends State<MyApp> {
 
   _togglemute() async {
     String url = "";
-    if(!_muteMusicSelected){
+    if (!_muteMusicSelected) {
       url = "http://${currentIP}:${currentPORT}/mute";
-    }else{
+    } else {
       url = "http://${currentIP}:${currentPORT}/unmute";
     }
 
-
     final response = await http.get(url);
-    setState((){ _muteMusicSelected = !_muteMusicSelected;});
+    setState(() {
+      _muteMusicSelected = !_muteMusicSelected;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-   	resizeToAvoidBottomPadding: false,
-        body: Center(
-          child: Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: ExactAssetImage("assets/24866.jpg"),
-                  fit: BoxFit.cover
+      resizeToAvoidBottomInset: false,
+      body: Center(
+        child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: ExactAssetImage("assets/24866.jpg"), fit: BoxFit.cover),
+          ),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 45, 15, 10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    RaisedButton.icon(
+                        onPressed: () {
+                          _putspleep();
+                        },
+                        icon: Icon(
+                          Icons.airline_seat_individual_suite_rounded,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Sleep',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.red.withOpacity(0.05)),
+                    RaisedButton.icon(
+                        onPressed: () {
+                          _shutdown();
+                        },
+                        icon: Icon(
+                          Icons.power_off,
+                          color: Colors.white,
+                        ),
+                        label: Text(
+                          'Power Off',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                        color: Colors.red.withOpacity(0.05)),
+                  ],
+                ),
               ),
-            ),
-            child: Column(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(15,45,15,10),
-                  child: Row(
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'VOLUMN',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          RaisedButton.icon(
+                              onPressed: () {
+                                _volumnDecrease();
+                              },
+                              icon: Icon(
+                                Icons.remove_circle,
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                'Down',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.red.withOpacity(0.05)),
+                          Text(' '),
+                          RaisedButton.icon(
+                              onPressed: () {
+                                _volumnIncrease();
+                              },
+                              icon: Icon(
+                                Icons.add_circle,
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                'Up',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.red.withOpacity(0.05)),
+                        ]),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'BRIGHTNESS',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          RaisedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.remove_circle,
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                'Down',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.red.withOpacity(0.05)),
+                          Text(' '),
+                          RaisedButton.icon(
+                              onPressed: () {},
+                              icon: Icon(
+                                Icons.add_circle,
+                                color: Colors.white,
+                              ),
+                              label: Text(
+                                'Up',
+                                style: TextStyle(color: Colors.white),
+                              ),
+                              color: Colors.red.withOpacity(0.05)),
+                        ]),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: () {
+                          _nextMedia();
+                        },
+                        icon: Icon(Icons.skip_next),
+                        color: Colors.white,
+                        iconSize: 34.0),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(85.0, 5, 85.0, 0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: () {
+                          _fastbackward();
+                        },
+                        icon: Icon(Icons.fast_rewind),
+                        color: Colors.white,
+                        iconSize: 34.0),
+                    IconButton(
+                        onPressed: () {
+                          _handlePlay();
+                        },
+                        icon: !_isPlaying
+                            ? Icon(Icons.chevron_right)
+                            : Icon(Icons.pause),
+                        color: Colors.white,
+                        iconSize: 34.0),
+                    IconButton(
+                        onPressed: () {
+                          _fastfoward();
+                        },
+                        icon: Icon(Icons.fast_forward),
+                        color: Colors.white,
+                        iconSize: 34.0),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(5.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    IconButton(
+                        onPressed: () {
+                          _previousMedia();
+                        },
+                        icon: Icon(Icons.skip_previous_sharp),
+                        color: Colors.white,
+                        iconSize: 34.0),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+                child: Divider(
+                  color: Colors.white,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: <Widget>[
-                      RaisedButton.icon(onPressed: (){ _putspleep(); }, icon: Icon(Icons.airline_seat_individual_suite_rounded, color: Colors.white,), label: Text('Sleep', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),
-                      RaisedButton.icon(onPressed: (){ _shutdown(); }, icon: Icon(Icons.power_off, color: Colors.white, ), label: Text('Power Off', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),
-                    ],
-                  ),
-                ),
-                Padding(padding: const EdgeInsets.fromLTRB(15,10,15,10),
-                  child: Divider(
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text('VOLUMN', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                      RaisedButton.icon(onPressed: (){ _volumnDecrease(); }, icon: Icon(Icons.remove_circle, color: Colors.white,), label: Text('Down', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),
-                      Text(' '),
-                      RaisedButton.icon(onPressed: (){ _volumnIncrease(); }, icon: Icon(Icons.add_circle, color: Colors.white,), label: Text('Up', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),]),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text('BRIGHTNESS', style: TextStyle(color: Colors.white,fontWeight: FontWeight.bold),),
-                      Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.remove_circle, color: Colors.white,), label: Text('Down', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),
-                      Text(' '),
-                      RaisedButton.icon(onPressed: (){}, icon: Icon(Icons.add_circle, color: Colors.white,), label: Text('Up', style: TextStyle(color: Colors.white),), color: Colors.red.withOpacity(0.05)),]),
-                    ],
-                  ),
-                ),
-                Padding(padding: const EdgeInsets.fromLTRB(15,10,15,10),
-                  child: Divider(
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      IconButton(onPressed: (){_nextMedia();}, icon: Icon(Icons.skip_next ), color: Colors.white, iconSize: 34.0),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(85.0,5,85.0,0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      IconButton(onPressed: (){ _fastbackward(); }, icon: Icon(Icons.fast_rewind ), color: Colors.white, iconSize: 34.0),
-                      IconButton(onPressed: (){ _handlePlay(); }, icon: !_isPlaying ? Icon(Icons.chevron_right ) : Icon(Icons.pause ), color: Colors.white, iconSize: 34.0),
-                      IconButton(onPressed: (){ _fastfoward(); }, icon: Icon(Icons.fast_forward ), color: Colors.white, iconSize: 34.0),
-                    ],
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(5.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      IconButton(onPressed: (){ _previousMedia(); }, icon: Icon(Icons.skip_previous_sharp ), color: Colors.white, iconSize: 34.0),
-                    ],
-                  ),
-                ),
-                Padding(padding: const EdgeInsets.fromLTRB(15,0,15,0),
-                  child: Divider(
-                    color: Colors.white,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                    IconButton(onPressed: (){ _togglemute(); }, icon: Icon(Icons.music_off ), color: _muteMusicSelected ? Colors.red : Colors.white, iconSize: 30.0),
-                    IconButton(onPressed: () async {
-                      String ip = await prompt(context);
-		      setState((){
-		      	currentIP = ip;
-		      });
-                    }, icon: Icon(Icons.headset ), color: Colors.white, iconSize: 30.0),
-                    IconButton(onPressed: () async {
-                      String ip = await prompt(context, title: Text('Enter host machine IP'),);
-		      setState((){
-		      	currentIP = ip;
-		      });
-                    },  icon: Icon(Icons.settings_rounded ), color: Colors.white, iconSize: 30.0),
+                      IconButton(
+                          onPressed: () {
+                            _togglemute();
+                          },
+                          icon: Icon(Icons.music_off),
+                          color: _muteMusicSelected ? Colors.red : Colors.white,
+                          iconSize: 30.0),
+                      IconButton(
+                          onPressed: () async {
+                            String ip = await prompt(context);
+                            setState(() {
+                              currentIP = ip;
+                            });
+                          },
+                          icon: Icon(Icons.headset),
+                          color: Colors.white,
+                          iconSize: 30.0),
+                      IconButton(
+                          onPressed: () async {
+                            String ip = await prompt(
+                              context,
+                              title: Text('Enter host machine IP'),
+                            );
+                            setState(() {
+                              currentIP = ip;
+                            });
+                          },
+                          icon: Icon(Icons.settings_rounded),
+                          color: Colors.white,
+                          iconSize: 30.0),
                     ]),
-                  ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
-
+      ),
+    );
   }
 }
